07d0a9b593c01837139090a2f331b63c73d473b8
Taylor McCoy <mccoyt57@gmail.com>
2023-10-04 13:06:43 -0500

    Enabled dark mode & created mode toggle switch for navbar

--- game_hub_project/src/Components/NavBar.tsx
+++ game_hub_project/src/Components/NavBar.tsx
@@ -1,11 +1,17 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, useColorMode, Switch } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { useEffect } from "react";
 
 const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <HStack>
+    <HStack display="flex" justifyContent="space-between">
       <Image src={logo} boxSize="60px" />
       <Text> This is the nav</Text>
+      <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode}>
+        {colorMode === "dark" ? "Dark Mode" : "Light Mode"}
+      </Switch>
     </HStack>
   );
 };
