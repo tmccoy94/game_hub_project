ea0f01bb9f75aff2ff072dea8d4f25965d47b4e7
Taylor McCoy <mccoyt57@gmail.com>
2023-10-04 08:58:09 -0500

    Created app, installed chakra ui

--- game_hub_project/src/App.tsx
+++ game_hub_project/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
 }
 
 export default App;
